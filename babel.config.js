a266fe1194bfd4b3b102653c1760832eb1696aca
0d1n92 <rombaldonienrico@gmail.com>
2023-08-05 15:05:23 +0200

    upd babel prod config

--- babel.config.js
+++ babel.config.js
@@ -4,12 +4,12 @@ module.exports = function (api) {
 
   const plugins = [];
 
-    plugins.push('transform-remove-console');
+    plugins.push(['transform-remove-console',{exclude: ['info']}]);
     plugins.push('const-enum');
 
 
   return {
-    presets: [['@babel/preset-typescript', { allowDeclareFields: true }]],
+    presets: [['@babel/preset-typescript', { allowDeclareFields: true, }]],
     plugins,
     comments: false
   };
