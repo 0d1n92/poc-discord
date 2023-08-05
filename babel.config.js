54b61268e52997885804f2369ccbe62919946cf5
0d1n92 <rombaldonienrico@gmail.com>
2023-08-05 14:57:23 +0200

    fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,16 @@
 
-require('dotenv/config');
-
 module.exports = function (api) {
-  const isProd = process.env.MODE === 'PROD';
-
   api.cache(true);
 
   const plugins = [];
-  if (isProd) {
+
     plugins.push('transform-remove-console');
-  }
+    plugins.push('const-enum');
+
 
   return {
     presets: [['@babel/preset-typescript', { allowDeclareFields: true }]],
     plugins,
+    comments: false
   };
 };
